0dc264d290a22c5e6604301c02369bc9087649bd
Kavita Kumari <54909292+kavita200496@users.noreply.github.com>
2020-07-12 20:33:29 +0530

    Create Merge Overlapping Intervals.cpp

---
+++ Array/Merge Overlapping Intervals.cpp
@@ -0,0 +1,35 @@
+https://www.interviewbit.com/problems/merge-overlapping-intervals/
+
+/**
+ * Definition for an interval.
+ * struct Interval {
+ *     int start;
+ *     int end;
+ *     Interval() : start(0), end(0) {}
+ *     Interval(int s, int e) : start(s), end(e) {}
+ * };
+ */
+ bool mycmp(Interval a, Interval b) {
+    
+    return a.start < b.start;
+} 
+vector<Interval> Solution::merge(vector<Interval> &intervals) {
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+
+    int n = intervals.size();
+    sort(intervals.begin(), intervals.end(), mycmp);
+    vector<Interval> res;
+    res.push_back(intervals[0]);
+    
+    for (int i = 1; i < n; i++) {
+        
+        if (intervals[i].start <= res.back().end) {
+            res.back().end = max(res.back().end, intervals[i].end);
+        } else res.push_back(intervals[i]);
+        
+    }
+    return res;
+}
